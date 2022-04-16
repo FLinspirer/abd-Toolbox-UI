c6d7ce5d35400eb8931eece2b289e1fc14114e9e
Tonyha7 <041202yuan@gmail.com>
2022-04-16 09:52:59 +0800

    v0.41

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,14 +10,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("abdUI")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © Tonyha7  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
 //对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
 //请将此类型的 ComVisible 特性设置为 true。
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("5cfa776c-3371-47d3-aed6-ae8cf6b16246")]
--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 只是一个对某管理程序逆向并用adb进行一些操作的小程序，大牛请绕道。
 
+为了提̶高̶逆̶向̶难̶度̶和减小程序体积，Release用了[ConfuserEx](https://github.com/mkaring/ConfuserEx)混淆。
+
 ### 请遵循GNU Lesser General Public License v2.1协议
